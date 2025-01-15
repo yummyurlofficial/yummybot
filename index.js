8b6c65d485682d822657166f2f5ca64b5a09c1b7
yummyurlofficial <yummyurlofficial@gmail.com>
2025-01-15 16:55:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -383,9 +383,9 @@ bot.onText(/\/footer(.*)/, (msg, match) => {
             chatId,
             `ᴛᴏ ᴀᴅᴅ ᴏʀ ᴜᴘᴅᴀᴛᴇ ʏᴏᴜʀ ꜱʜᴏʀᴛɴᴇʀ ᴡᴇʙꜱɪᴛᴇ ᴀᴘɪ.\n\n` 
             
-            `Ex: /api a11b67871d303d2e0f76ab0:\n` 
+            `Ex: /api a11b67871d303d2e0f76ab0:\n\n` 
             
-            `ɢᴇᴛ ᴀᴘɪ ꜰʀᴏᴍ Yummyurl.com <br>
+            `ɢᴇᴛ ᴀᴘɪ ꜰʀᴏᴍ Yummyurl.com \n\n<br>
             
             ᴄᴜʀʀᴇɴᴛ yummyurl.com ᴀᴘɪ: None`
           );
